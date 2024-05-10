4b714e3f89a48df40af0931b761062b711808ccb
KavyaMoturi <142881626+KavyaMoturi@users.noreply.github.com>
2024-05-10 11:34:56 +0530

    Update armstrong-numbers.cpp

--- Easy/Armstrong Numbers/armstrong-numbers.cpp
+++ Easy/Armstrong Numbers/armstrong-numbers.cpp
@@ -8,19 +8,22 @@ using namespace std;
 class Solution {
   public:
     string armstrongNumber(int n){
-        // code here
+        // declaration of variables
         int sum=0;
         int t=n;
       while(t){
+        //rem variable gives us the remainder which are individual digits of given number
           int rem = t%10;
+          //sum of cube of individual digits
           sum= sum+ (rem*rem*rem);
           t=t/10;
       }
-      
+      //if sum is equal to given number then it is an armstrong number
       if (sum == n){ 
           
           return "Yes";
       }
+        //or else not an armstrong number
       else {
           
           return "No";
