618b84eeff819efe40761eb85a2ef587095888fd
KavyaMoturi <142881626+KavyaMoturi@users.noreply.github.com>
2024-09-24 15:10:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Mean/mean.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int mean(int N , int A[]) {
+        // code here
+        int mean=0;
+        for(int i=0;i<N;i++){
+            mean+=A[i];
+        }
+        return mean/N;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.mean(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
